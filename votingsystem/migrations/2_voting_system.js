f2e4421014a95d646450e05d14a3621c68326939
soham shrikant manjrekar <91745711+sohammanjrekar@users.noreply.github.com>
2023-10-26 22:49:02 +0530

    upload

--- votingsystem/migrations/2_voting_system.js
+++
@@ -1,5 +0,0 @@
-const YourContract = artifacts.require("IndianElectionSystem"); // Replace "YourContract" with your contract's name
-
-module.exports = function (deployer) {
-  deployer.deploy(YourContract);
-};
